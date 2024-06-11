a3f85021c2e0a7f33eaef71c823b58db0b10c757
mvpodell <mvpa2020@mymail.pomona.edu>
2024-06-11 15:16:11 -0700

    about page work

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,19 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className={styles.banner}>
           We are closed for the summer! See you in the fall!
         </div>
-        <div className={styles.dashHeader}>
-          <div className={styles.subtitle}>Welcome to the</div>
-          <div className={styles.boldTitle}>OEC</div>
-          <div className={styles.subtitle2}>
-            The Outdoor Education Center of Pomona College
-          </div>
-          <div className={styles.dashHours}>
-            <div>Hours:</div>
-            <div>M-Th: 2pm-8pm</div>
-            <div>Friday: 10am-1pm</div>
-            <div>Sat-Sun: CLOSED</div>
-          </div>
-        </div>
+
         <div className={styles.body}>{children}</div>
       </div>
     </div>
